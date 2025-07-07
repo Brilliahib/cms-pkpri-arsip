bcfe1a1f7b809f7027bafec6d3db09b742aba313
brilliahib <brilliahib21@gmail.com>
2025-07-07 17:25:42 +0700

    feat: view and delete actions

--- app/Filament/Resources/OtherMailResource.php
+++ app/Filament/Resources/OtherMailResource.php
@@ -101,7 +101,9 @@ class OtherMailResource extends Resource
                     }),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
